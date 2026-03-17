c08ba0651514b55b46e8c6f45e0e4a17fba1a835
Aysha Nashra <ayshasblooms@gmail.com>
2026-03-17 21:55:51 +0530

    m

--- backend/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRecipeSummary } from "../controllers/aiController.js";
-
-const router = express.Router();
-
-router.post("/summary", getRecipeSummary);
-
-export default router;
