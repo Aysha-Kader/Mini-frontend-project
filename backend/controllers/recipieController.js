31ae1512396969373c3b78c0556002951926cc3a
Aysha Nashra <ayshasblooms@gmail.com>
2026-03-29 09:06:10 +0530

    m

--- backend/controllers/recipieController.js
+++ backend/controllers/recipieController.js
@@ -110,6 +110,12 @@ export const updateRecipe = async (req, res) => {
 //add feedback
 export const addFeedback = async (req, res) => {
   try {
+    console.log(recipe.user);
+                    console.log(user);
+                    console.log(localStorage.getItem("token"));
+                    console.log(req.params.id);
+                    console.log(error);
+
     const recipe = await Recipe.findById(req.params.id);
 
     if (!recipe) {
