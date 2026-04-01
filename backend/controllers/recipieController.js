9738916fb7bb86b2bbc784459b1ed96d7bca8af0
Aysha Nashra <ayshasblooms@gmail.com>
2026-04-01 21:24:56 +0530

    m

--- backend/controllers/recipieController.js
+++ backend/controllers/recipieController.js
@@ -180,7 +180,7 @@ export const deleteFeedback = async (req, res) => {
   }
 
   if (
-    feedback.user.toString() !== req.user
+    feedback.user.toString() !== req.user && req.user.role !== "admin"
     
   ) {
     return res.status(403).json({
