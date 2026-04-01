c676f6d8dddaad44bb2d2f309e29d5d29ddfdfb6
Aysha Nashra <ayshasblooms@gmail.com>
2026-04-01 07:35:01 +0530

    m

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -24,7 +24,7 @@ const recipeSchema = new mongoose.Schema({
   instructions: String,
   diet: String,
   difficulty: String,
-  cookTime: Number,
+  cookTime:String,
    feedbacks: [feedbackSchema],
 },
 
